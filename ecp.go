a20cee6dbb11f7519cadfe0c641de642c5cdf9c1
wrfly <mr.wrfly@gmail.com>
2018-12-18 00:07:05 +0800

    typo

--- ecp.go
+++ ecp.go
@@ -144,7 +144,6 @@ func rangeOver(config interface{}, parseDefault,
 			continue
 		}
 
-
 		switch kind {
 		case reflect.String:
 			if field.String() != "" && !exist {
@@ -243,7 +242,7 @@ func rangeOver(config interface{}, parseDefault,
 }
 
 // note Parse function will overwrite the existing value if there is a
-// envitonment configration matched with the struct name or the "env" tag
+// environment configration matched with the struct name or the "env" tag
 // name.
 
 // Parse the configuration through environments starting with the prefix
@@ -269,7 +268,7 @@ func Parse(config interface{}, prefix ...string) error {
 //
 // then you can use ecp.Default(&c) to parse the default value to the struct.
 // note, the Default function will not overwrite the existing value, if the
-// config key has already been set nomatter whether it has a default tag.
+// config key has already been set no matter whether it has a default tag.
 // And the default value will be nil (nil of the type) if the "default" tag is
 // empty.
 
@@ -279,7 +278,7 @@ func Default(config interface{}) error {
 	return err
 }
 
-// List function will also fill up the value of the envitonment key
+// List function will also fill up the value of the environment key
 // it the "default" tag has value
 
 // List all the config environments
